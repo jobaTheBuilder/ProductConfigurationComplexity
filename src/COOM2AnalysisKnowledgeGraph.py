303fe93424c3f39f4b2f703887447aaea167c034
Joachim Baumeister <joachim.baumeister@denkbares.com>
2026-03-18 17:06:17 +0100

    Inital stub of terminology conversion

---
+++ src/COOM2AnalysisKnowledgeGraph.py
@@ -0,0 +1,95 @@
+import argparse
+from pathlib import Path
+
+from rdflib import Graph, Namespace, RDF, URIRef
+from rdflib.namespace import split_uri
+from rdflib.collection import Collection
+
+class COOM2AnalysisKnowledgeGraph:
+    def __init__(self, input_path: Path):
+        self.COOM = Namespace("http://www.denkbares.com/coomV2#")
+        self.EX = Namespace("http://www.example.org/ns#")
+        self.FEATURE = self.COOM["Feature"]
+        self.COMPONENT = self.COOM["Structure"]
+        self.COOM_TYPE = self.COOM["type"]
+
+        self.EX_COMPONENT = self.EX["COMPONENT"]
+        self.EX_FEATURE = self.EX["FEATURE"]
+        self.EX_IN = self.EX["in"]
+        self.EX_HAS = self.EX["has"]
+
+        self.EX_VALUE = self.EX["VALUE"]
+        self.EX_KNOWLEDGE = self.EX["KNOWLEDGE"]
+
+        self.input_graph = Graph()
+        self.input_graph.parse(input_path)
+
+    def export(self, out_path: Path):
+        output_graph = Graph()
+        output_graph.bind("", self.EX)
+        self._generate_features(output_graph)
+        self._generate_components(output_graph)
+        self._generate_knowledge(output_graph)
+        output_graph.serialize(out_path)
+
+    def _generate_components(self, output_graph):
+        for c in self.input_graph.subjects(RDF.type, self.COMPONENT):
+            # BUILDING RULE Component Node (CN)
+            namespace, local_name = split_uri(str(c))
+            cv = URIRef(self.EX + local_name)
+            output_graph.add((cv, RDF.type, self.EX_COMPONENT))
+
+            # BUILDING RULE Hierarchy Edge (SE1 / SE2)
+            feature_list_node = self.input_graph.value(c, self.COOM["hasFeature"])
+            features = Collection(self.input_graph, feature_list_node)
+            for feature in features:
+                namespace, local_name = split_uri(str(feature))
+                fv = URIRef(self.EX + local_name)
+                output_graph.add((cv, self.EX_HAS, fv))
+                # todo BikeT_bag
+
+    def _generate_features(self, output_graph: Graph):
+        for f in self.input_graph.subjects(RDF.type, self.FEATURE):
+            # BUILDING RULE: FEATURE NODE (FN)
+            namespace, local_name = split_uri(str(f))
+            fv = URIRef(self.EX + local_name)
+            output_graph.add((fv, RDF.type, self.EX_FEATURE))
+
+            # BUILDING RULE Feature Value Edge (FE)
+            feature_options = self._get_values_of(f)
+            if feature_options:
+                for option in feature_options:
+                    output_graph.add((fv, self.EX_HAS, URIRef(self.EX + option)))
+
+    def _generate_knowledge(self, output_graph: Graph):
+        # todo: build knowledge elements
+        pass
+
+    def _get_values_of(self, f) -> list[str]:
+        the_type = self.input_graph.value(f, self.COOM_TYPE)
+        if the_type == self.COOM["Boolean"]:
+            return ["true", "false"]
+        elif the_type == self.COOM["Numerical"]:
+            return ["numerical_range"]
+        elif the_type == self.COOM["Textual"]:
+            return ["textual_range"]
+        else:
+            options_node = self.input_graph.value(the_type, self.COOM["hasOption"])
+            options = Collection(self.input_graph, options_node)
+            str_options = []
+            for option in options:
+                namespace, local_name = split_uri(str(option))
+                str_options.append(local_name)
+            return str_options
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Example app to convert COOM RDF to Analysis Knowledge Graph.")
+    parser.add_argument("-i", "--inputfile", help="Input COOM RDF file", required=True)
+    parser.add_argument("-o", "--outfile", help="Output RDF file.", required=True)
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    app = COOM2AnalysisKnowledgeGraph(Path(args.inputfile))
+    app.export(Path(args.outfile))
